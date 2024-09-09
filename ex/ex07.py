3da3213a51ca230c5dbc6cf20dac4d94fc5ffc69
김재헌 <skiel20141209@gmail.com>
2024-09-09 12:10:23 +0900

    1234

--- ex/ex07.py
+++ ex/ex07.py
@@ -11,19 +11,24 @@ print(f'입력된 주민번호 = {b}')
 
 def count_jumin(b):
     birth = int(b[:2])
-    gender = int(b[7])
+    gender = int(b[6])
     if gender in [1, 2] :
-        c = birth + 1900
+        c = 2024 - (birth + 1900)
     elif gender in [3, 4] : 
-        c = birth + 2000
+        c = 2024 - (birth + 2000)
+    else : 
+        print('주민등록번호 형식이 아닙니다.')
+        exit()
     
     if gender in [1, 3] : 
         d = '남자'
     elif gender in [2, 4] :
         d = '여자'
-    
+    else :
+        print('주민등록번호 형식이 아닙니다.')
+        exit()
     
     return c, d
 
 c, d = count_jumin(b)
-print(f'{c}년생, {d}')
+print(f'{c}살, {d}')
