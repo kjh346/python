a272551f6d688d3bef5a33beb584db9a74d531fc
김재헌 <skiel20141209@gmail.com>
2024-09-12 16:35:53 +0900

    1234

--- 05_객체.py
+++ 05_객체.py
@@ -1,7 +1,22 @@
 class 사람 :
-    def cry(self) :
-        print('응애응애')
+    def cry(self, 소리) :
+        for 변수 in 소리 :
+            print(변수)
+            
+human=사람()
+            
+human.cry('엉엉')
 
-human = 사람() 
+class 사람1 : 
+    def call(self, 이름, 번호) : 
+        print(f'{이름}에게 {번호} 발신중')
+        
+human1 = 사람1()
+human1.call('김철수','010-1234-5678')
 
-human.cry()
+class 사람2 : 
+    def 개인정보(self, name) :
+        print(name[0] + (len(name)-2)*'x' + name[-1])
+        
+human2 = 사람2()
+human2.개인정보('유빛나라')
---
+++ 06_생성자.py
@@ -0,0 +1,17 @@
+class Human : 
+    def __init__(self) :
+        print('응애응애')
+        
+human = Human()
+
+class 사람 : 
+    def __init__(self, name) : 
+        print(f'{name} 님이 태어났습니다.')
+    
+human_ = 사람('김재헌')
+
+class 사람1 : 
+    def __init__(self, 이름, 연도) : 
+        self.연도 = 연도
+        
+human1 = 사람1('김재헌', 1995)
